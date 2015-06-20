3116defae0c2f07054df7714390f4293aec233d4
Matt Biggers <mpbiggers@gmail.com>
2015-06-20 13:04:12 -0700

    completed fibonacci series code, working on next steps

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -1,1 +1,11 @@
-def f
+print "Let's experiment with the Fibonacci Sequence."
+
+n = raw_input("Please type any number. Don't include any commas! > ")
+i = int(n)
+
+
+def fib(i):
+    x = (i - 1) + i
+    return x
+
+print fib(i)
