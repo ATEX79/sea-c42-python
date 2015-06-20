53e80e5715cc458a4edc1291b3861f3c28d1cb8f
Matt Biggers <mpbiggers@gmail.com>
2015-06-20 14:16:00 -0700

    corrected fibonacci code, created lucas code

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -1,11 +1,39 @@
-print "Let's experiment with the Fibonacci Sequence."
+print "Let's experiment with the Fibonacci and Lucas Numbers Series."
 
-n = raw_input("Please type any number. Don't include any commas! > ")
+# Fibonacci series
+
+print "First let's work with the Fibonacci Series."
+
+n = raw_input("Please type any number. Don't include any commas or negative numbers! > ")
 i = int(n)
 
 
-def fib(i):
-    x = (i - 1) + i
-    return x
+def fibonacci(i):
+    if (i == 0):
+        return(0)
+    elif (i == 1):
+        return(1)
+    elif (i > 1):
+        return(fibonacci(i - 1) + fibonacci(i - 2))
 
-print fib(i)
+# should I call the function now or later in "part 3" or "finally" section?
+print fibonacci(i)
+
+# Lucas Numbers
+
+print "Now let's work with Lucas Numbers Series."
+
+l = raw_input("Please type any number and don't use commas or negative numbers. > ")
+m = int(l)
+
+
+def lucas(m):
+    if (m == 1):
+        return(2)
+    elif (m == 2):
+        return(1)
+    elif (m > 2):
+        return(lucas(m - 1) + lucas(m - 2))
+
+# should I call this function now or later?
+print lucas(m)
