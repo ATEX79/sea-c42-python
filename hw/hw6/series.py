6881bccea941c23f8b7e293b3c6a4900abc8f244
Matt Biggers <mpbiggers@gmail.com>
2015-06-22 09:02:58 -0700

    updated sum_series function on hw6, but still not working

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -7,7 +7,7 @@ print ("Let's experiment with the Fibonacci and Lucas Numbers Series.")
 
 print ("First let's work with the Fibonacci Series.")
 
-n = raw_input("Please type any number. Don't include any commas or negative numbers! > ")
+n = input("Please type any number. Don't include any commas or negative numbers. > ")
 i = int(n)
 
 
@@ -19,18 +19,18 @@ def fibonacci(i):
     elif (i > 1):
         return(fibonacci(i - 1) + fibonacci(i - 2))
 
-# should I call the function now or later in "part 3" or "finally" section?
-# print fibonacci(i)
+
+print (fibonacci(i))
 
 # Lucas Numbers
 
 print ("Now let's work with Lucas Numbers Series.")
 
-l = raw_input("Please type any number and don't use commas or negative numbers. > ")
+l = input("Please type any number and don't use commas or negative numbers. > ")
 m = int(l)
 
 
-def lucas(m, ):
+def lucas(m):
     if (m == 0):
         return(2)
     elif (m == 1):
@@ -38,27 +38,21 @@ def lucas(m, ):
     elif (m > 1):
         return(lucas(m - 1) + lucas(m - 2))
 
-# should I call this function now or in part 3 or "finally"?
-print lucas(m)
+print (lucas(m))
 
 
-# part 3 "sum series" - this is where I'm not sure how to proceed. ignore the code.
-# it's a rough draft, incomplete, and incorrect.
+# Sum Series
 
-r = raw_input("Please call the Fibonacci function by typing in one paramater.  Call the Lucas function by typing in one paramater + 2, 1. > ")
+r = input("Let's call the Fibonacci and Lucas functions again by typing in one paramater. > ")
 s = int(r)
 
 
-"""def sum_series(s, *args):
-       if (b == 0):
+def sum_series(s, x=0, y=1):
+    if (x == 0):
         return(0)
-    elif (i == 1):
+    elif (y == 1):
         return(1)
-    elif (i > 1):
-        return(fibonacci(i - 1) + fibonacci(i - 2))
+    elif (s > 1):
+        return(sum_series(s - 1) + sum_series(s - 2))
 
-def sum_series (m, )"""
-
-def sum_series(s, *args):
-    if args = 2 or args = 1:
-        print lucas(s, args)
+print ("Fibonacci:", sum_series(s), "Lucas:", sum_series(s, x=2, y=1))
