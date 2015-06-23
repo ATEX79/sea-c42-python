f3ba50c0c9fd8c14997e4636dd497f89baa6343c
Matt Biggers <mpbiggers@gmail.com>
2015-06-22 18:49:24 -0700

    completed HW6

--- hw/hw6/series.py
+++ hw/hw6/series.py
@@ -1,13 +1,10 @@
-# is import sys necessary?
-# import sys
-
-"""print ("Let's experiment with the Fibonacci and Lucas Numbers Series.")
+print ("Let's experiment with the Fibonacci and Lucas Numbers Series.")
 
 # Fibonacci series
 
 print ("First let's work with the Fibonacci Series.")
 
-n = input("Please type any number. Don't include any commas or negative numbers. > ")
+n = input("Please type any number with no commas or negative numbers. > ")
 i = int(n)
 
 
@@ -26,7 +23,7 @@ print (fibonacci(i))
 
 print ("Now let's work with Lucas Numbers Series.")
 
-l = input("Please type any number and don't use commas or negative numbers. > ")
+l = input("Please type any number with no commas or negative numbers. > ")
 m = int(l)
 
 
@@ -38,34 +35,20 @@ def lucas(m):
     elif (m > 1):
         return(lucas(m - 1) + lucas(m - 2))
 
-print (lucas(m))"""
+print (lucas(m))
 
 
 # Sum Series
 
-r = input("Let's call the Fibonacci (using default values) and Lucas (using 2 and 1 as optional parameters) functions again by typing in one number. > ")
+r = input("Let's call the sum_series function. Please type any number; no negatives or commas. > ")
 s = int(r)
 
-
-"""def sum_series(s, x=0, y=1):
-    if (s == 0 and x == 0):
-        return(0)
-    if (s == 1 and y=):
-        return (2)
-    if (s == 1):
-        return(1)
-    elif (s > 1):
-        return(sum_series(s - 1) + sum_series(s - 2))"""
-
-
+"""This function calls either the Fibonacci or Lucas function based on optional parameters."""
 def sum_series(s, x=0, y=1):
-    print (fibonacci(s, 0, 1))
+    if (x == 0 and y == 1):
+        return(fibonacci(s))
+    if (x == 2 and y == 1):
+        return(lucas(s))
 
-
-def sum_series_luc(s, x=2, y=1):
-    print (lucas(s, 2, 1))
-
-# Fibonacci
-# print (sum_series(s))
-# Lucas
-# print (sum_series(s, 2, 1))
+print ("Fibonacci Series:", sum_series(s))
+print ("Lucas Series:", sum_series(s, x=2, y=1))
